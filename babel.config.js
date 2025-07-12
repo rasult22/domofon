3de25697ca0eb750cf73c6ccc08e1d99cf4c7d75
rasult22 <warrior.006@inbox.ru>
2025-07-12 05:28:08 +0500

    refactor: update babel and metro configs for nativewind
    - Replace @babel/preset-react with nativewind/babel preset
    - Clean up metro config comments and formatting
    - Fix text color and typo in index.tsx

--- babel.config.js
+++ babel.config.js
@@ -2,8 +2,8 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      ['babel-preset-expo', {jsxImportSource: 'nativewind'}],
-      ['@babel/preset-react', {runtime: 'automatic'}],
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
     ],
   };
 };
