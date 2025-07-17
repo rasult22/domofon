c5f079416f4f5a878fec82f695dd5e34b582d7a3
rasult22 <warrior.006@inbox.ru>
2025-07-17 15:07:28 +0500

    feat(webrtc): implement webrtc call handling with react-query
    - Add react-query for state management
    - Implement call handling with pocketbase backend
    - Replace native client with new webrtc implementation
    - Add required dependencies including react-native-webrtc

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,22 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 import { StatusBar, useColorScheme } from "react-native";
+import EventSource from "react-native-sse";
+import queryClient from '.././queries/client';
 import '../global.css';
 
 export default function RootLayout() {
   const isDarkMode = useColorScheme() === "dark";
+  if (!global.EventSource) {
+    (EventSource as any).CONNECTING = 0;
+    (EventSource as any).OPEN = 1;
+    (EventSource as any).CLOSED = 2;
+    global.EventSource = EventSource as typeof global.EventSource;
+  }
 
   return (
     <>
+    <QueryClientProvider client={queryClient}>
       <StatusBar barStyle={isDarkMode ? "light-content" : "dark-content"} />
       <Stack screenOptions={{
         contentStyle: {
@@ -15,6 +25,7 @@ export default function RootLayout() {
       }}>
         <Stack.Screen name="index" options={{ headerShown: false }} />
       </Stack>
+    </QueryClientProvider>
     </>
   );
 }
